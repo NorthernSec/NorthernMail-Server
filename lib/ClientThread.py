bd9f6c223bfb027e2e2176009a9e4a2ab453c486
PidgeyL <pieterjan.moreels@gmail.com>
2015-04-27 17:51:05 +0200

    placeholders for encryption and data handling

--- lib/ClientThread.py
+++ lib/ClientThread.py
@@ -21,15 +21,30 @@ class ClientThread(threading.Thread):
     self.port=port
     self.csocket=clientsocket
     self.logger=RSALogger()
-
+    self.AESKey=None
   def run(self):
     try:
       self.logger.log('Accepting connection from %s:%s'%(self.ip,self.port),1)
       data='temp'
       while True and len(data)>0:
         data=self.csocket.recv(2048)
-        #handle data
+        response=handleData(data)
+        if response:
+          csocket.send(encrypt(response))
     finally:
       self.logger.log('Closing connection from %s:%s, sending last data'%(self.ip,self.port))
       self.csocket.shutdown(socket.SHUT_RDWR)
       self.logger.log('Closed connection from %s:%s'%(self.ip,self.port),1)
+
+  def encrypt(data):
+    if self.AESKey:
+      #TODO: handle encryption
+      print("Encryption to do")
+    data=data.encode('utf-8')
+    return data
+
+  def handleData(data):
+    d=data.upper()
+    if d.startswith("FETCH"):
+      #TODO: handle mails
+      return "Placeholder for mails"
